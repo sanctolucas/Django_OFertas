a4dec42f35d59810649c39af8ff920754bc907f1
Santos Lucas <santoslucs@icloud.com>
2023-10-21 23:04:40 -0300

    Create context, title and content to home page, about page and contact page. Update src/template/home/home_page.html, src/template/about/view.html, src/template/contact/view.html to use context variables

--- DjangoOfertas/views.py
+++ DjangoOfertas/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home_page(request):
     context = {
         "title": "Página principal",
         "content": "Bem-vindo a página principal"
     }
-    return render(request, "home_page.html", context)
+    return render(request, "home/home_page.html", context)
 
 def about_page(request):
     context = {
